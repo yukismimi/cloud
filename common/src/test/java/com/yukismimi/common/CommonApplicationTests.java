49a2bbe69980c6a873eed5cdb8fd85986aaf14c1
Yukismimi <fz_saber@qq.com>
2020-03-20 00:13:17 +0800

    foo

---
+++ common/src/test/java/com/yukismimi/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukismimi.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
